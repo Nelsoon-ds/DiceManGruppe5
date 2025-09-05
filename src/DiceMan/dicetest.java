fdc3c60efa7926f247145f45ee23c53635b728b6
AsiaAlGhazali <asiaalghazali@gmail.com>
2025-09-05 15:24:42 +0200

    Flyttet pakke

--- src/DiceMan/dicetest.java
+++ src/DiceMan/dicetest.java
@@ -1,8 +1,10 @@
+package DiceMan;
+
 public class dicetest {
     public static void main(String[] args) {
         //opret en DiceMan med navn
-        Dice dice =new Dice();
-        DiceMan diceman =new DiceMan();
+        Dice dice = new Dice();
+        DiceMan diceman = new DiceMan();
 
         //starter dagen - han ruller 5 gange eller stopper tideligt
         diceman.startDag();
