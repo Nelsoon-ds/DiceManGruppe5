4601c5ef3eedb8c6baac409734bcc267c4a4ab4b
AsiaAlGhazali <asiaalghazali@gmail.com>
2025-09-05 15:40:15 +0200

    senest

--- src/DiceMan/dicetest.java
+++ src/DiceMan/dicetest.java
@@ -4,10 +4,10 @@ public class dicetest {
     public static void main(String[] args) {
         //opret en DiceMan med navn
         Dice dice = new Dice();
-        DiceMan diceman = new DiceMan();
+        DiceMan diceman = new DiceMan(2,3,"test");
 
         //starter dagen - han ruller 5 gange eller stopper tideligt
-        diceman.startDag();
+        diceman.startDay();
 
     }
 }
