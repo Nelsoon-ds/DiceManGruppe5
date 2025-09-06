581e931f26b756d6fc897aae4466b83bb9a2513d
Esp <duckerlicious@duck.com>
2025-09-06 14:48:06 +0200

    delt startDay metoden op i to, og gjort output en lille smule mere overskueligt

--- src/DiceMan/DiceMan.java
+++ src/DiceMan/DiceMan.java
@@ -1,44 +1,50 @@
 package DiceMan;
 
 public class DiceMan {
-    private int antalAktiviteter = 0;
+    private int activityCount = 0;
     private int counter = 0;
 
     Dice dice = new Dice();
 
     public void startDay() {
-        System.out.println("DiceMan står op og ved ikke hvad han skal tage sig til i dag.");
-        while (antalAktiviteter <= 4) {
-            int diceResult = dice.rollDice();
+        System.out.println("DiceMan står op og ved ikke hvad han skal tage sig til i dag.\n");
+        while (activityCount <= 4) {
 
             // Vi tjækker om han er gået tilbage i seng 3 gange endnu
             if (counter >= 3) {
                 System.out.println("DiceMan blev for doven og blev i sengen for evigt...");
-                antalAktiviteter = antalAktiviteter + 3;
+                activityCount = activityCount + 3;
             } else {
-                switch (diceResult) {
-                    case 1:
-                        System.out.println("han spiser morgenmad");
-                        break;
-                    case 2:
-                        System.out.println("han læser op på noget kedeligt");
-                        break;
-                    case 3:
-                        System.out.println("han svømmer en tur");
-                        break;
-                    case 4:
-                        System.out.println("han tager ud og fisker");
-                        break;
-                    case 5:
-                        System.out.println("han ringer til mor");
-                        break;
-                    case 6:
-                        System.out.println("han går tilbage i seng");
-                        counter++;
-                        break;
-                }
-                antalAktiviteter++;
+                whichActivity();
             }
         }
+    }
+
+    public void whichActivity() {
+        int diceResult = dice.rollDice();
+        //int diceResult = 6;
+
+        switch (diceResult) {
+            case 1:
+                System.out.println("Han spiser morgenmad");
+                break;
+            case 2:
+                System.out.println("Han læser op på noget kedeligt");
+                break;
+            case 3:
+                System.out.println("Han svømmer en tur");
+                break;
+            case 4:
+                System.out.println("Han tager ud og fisker");
+                break;
+            case 5:
+                System.out.println("Han ringer til mor");
+                break;
+            case 6:
+                System.out.println("Han går tilbage i seng");
+                counter++;
+                break;
+        }
+        activityCount++;
     }
 }
